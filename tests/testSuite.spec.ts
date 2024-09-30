8762261a15b55ebfc0d63555d362bb8330344ba7
Enamulbd1 <enamulbd1.gmail.com>
2024-09-30 11:12:31 +0200

    Delete car request

--- tests/testSuite.spec.ts
+++ tests/testSuite.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { faker} from "@faker-js/faker"
-import { json } from 'stream/consumers';
-import exp from 'constants';
+
+
 
 
 test.describe('Test Suite 01- car rental test', () => {
@@ -13,9 +13,9 @@ test.describe('Test Suite 01- car rental test', () => {
 
    test('Test case 02 - create post to the cars', async ({ request }) => {
     
-    const uniqueRegistrationNumber = faker.vehicle.vin(); // Generates a random number for id
+    const uniqueRegistrationNumber = faker.vehicle.vin();
     const carDetails = { 
-      pricePerDay: parseFloat(faker.finance.amount()),  // Generates a random price
+      pricePerDay: parseFloat(faker.finance.amount()),
       fabric: faker.vehicle.color(),
       model: faker.vehicle.model(),
       registrationNumber: uniqueRegistrationNumber,
@@ -57,7 +57,7 @@ test('Test case 04 - create post to add a customer', async ({ request }) => {
     phoneNumber: faker.phone.number(),          
   };
 
-  // Send a POST request to add a new customer
+  //  POST request to add a new customer
   const createCustomerResponse = await request.post('http://localhost:9090/api/v1/addcustomer', {
     headers: {
       'Content-Type': 'application/json',
@@ -76,16 +76,87 @@ test('Test case 04 - create post to add a customer', async ({ request }) => {
   });
 });
 
-test('Test case 05- delete a car by ID', async ({ request }) => {
-  const getPostsResponse = await request.get('http://localhost:9090/api/v1/deletecar');
-  expect (getPostsResponse.ok()).toBeTruthy();
-  const allCars = await getPostsResponse.json();
-  const lastButOneCarID = allCars[allCars.length - 2].id;
-  // expect ( getPostsResponse.status()).toBe(200);
-})
+test('Test case 05 - delete a car by ID', async ({ request }) => {
+  // Get the list of all cars
+  const getPostsResponse = await request.get('http://localhost:9090/api/v1/allcars');
+  expect(getPostsResponse.ok()).toBeTruthy();
+  
+   const allCars = await getPostsResponse.json();
 
+   const lastButOnePostID = allCars[allCars.length - 2].id;
+  
+   const deletePostResponse = await request.delete(`http://localhost:9090/api/v1/deletecar/${lastButOnePostID}`);
+   expect(deletePostResponse.status()).toBe(404);
 
+   const getDeletedCarResponse = await request.get(`http://localhost:9090/api/v1/getcar/${lastButOnePostID}`);
+   expect(getDeletedCarResponse.status()).toBe(404);
+  
   
 });
+
+test('Test case 06 - update a car by ID', async ({ request }) => {
+  const getAllCustomers = await request.get('http://localhost:9090/api/v1/allcars');
+  expect(getAllCustomers.ok()).toBeTruthy();
+
+  const allCars = await getAllCustomers.json();
+  const customerToUpdate = allCars[1];
+  const customerID = customerToUpdate.id;
+
+  // new data for the car update
+  const updatedCustomerDetails = {
+    id: customerID,
+    pricePerDay: parseFloat(faker.finance.amount()),
+    fabric: faker.vehicle.color(),     
+    model: faker.vehicle.model(), 
+    registrationNumber: faker.vehicle.vin(),
+    isBooked: false
+  };
+
+  const updateCustomerResponse = await request.put("http://localhost:9090/api/v1/updatecar", {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: JSON.stringify(updatedCustomerDetails), // Passing the updated car details in the request body
+  });
+   expect(updateCustomerResponse.ok()).toBeTruthy();
+
+   const getUpdatedCarResponse = await request.get("http://localhost:9090/api/v1/allcars");
+  expect(getUpdatedCarResponse.ok()).toBeTruthy();
+
+});
+
+test('Test case 07 - update a customer by ID', async ({ request }) => {
+  const getAllCustomers = await request.get('http://localhost:9090/api/v1/customers');
+  expect(getAllCustomers.ok()).toBeTruthy();
+
+  const allCustomers = await getAllCustomers.json();
+  const customerToUpdate = allCustomers[5];
+  const customerID = customerToUpdate.id;
+
+  // new data for the car update
+  const updatedCustomerDetails = {
+    id: customerID,
+    username: "Mehmet01",
+    name: "Mehmet",
+    address: "sollentuna",
+    email: "mehmet@gmail.com",
+   phoneNumber: "0763908046"
+  };
+
+  const updateCustomerResponse = await request.put("http://localhost:9090/api/v1/updatecustomer", {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: JSON.stringify(updatedCustomerDetails), // Passing the updated car details in the request body
+  });
+   expect(updateCustomerResponse.ok()).toBeTruthy();
+
+  const getUpdatedCustomerResponse = await request.get("http://localhost:9090/api/v1/customers");
+  expect(getUpdatedCustomerResponse.ok()).toBeTruthy();
+
+});
+
+});
+
 
 
